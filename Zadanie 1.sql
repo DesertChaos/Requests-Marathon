35bb4e37cab65c11f575cd2f3b5b71bc15dd36d0
DesertChaos <vadimsysolin52565@gmail.com>
2024-10-17 15:23:54 +0300

    Update Zadanie 1.sql

--- Zadanie 1.sql
+++ Zadanie 1.sql
@@ -1,4 +1,8 @@
-select u."FirstName", u."LastName", r."CountryCode"
-from "User" u
-cross JOIN "Runner" r
-cross JOIN "Marathon" m;
+select distinct "FirstName", "LastName", "BibNumber",r2."CountryCode"
+from "Registration" r 
+join "Runner" r2 on r."RunnerId" = r2."RunnerId" 
+join "User" u on r2."Email" = u."Email" 
+join "RegistrationEvent" re on r."RegistrationId" = re."RegistrationId" 
+join "Event" e on re."EventId" = e."EventId" 
+join "Marathon" m on e."MarathonId" = m."MarathonId" 
+where m."MarathonId" = 5
